fda044cf22c2f4e1dba038a8969865975e46841d
Folashade-cloud <ftogunajo@gmail.com>
2022-09-15 11:59:05 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,33 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabets in small letters
+ * print_alphabet_x10 - function that prints 10 times the alphabet,in lowercase
  *
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
+	int counter = 1;
 
-	char alpha = 'a';
+	while (counter <= 10)
 
-	while (alpha <= 'z')
 	{
-		_putchar(alpha);
-		alpha++;
+
+		char alpha;
+
+		char alpha_end = 'z';
+
+		for (alpha = 'a'; alpha <= alpha_end; alpha++)
+
+		{
+			_putchar(alpha);
+
+		}
+			_putchar('\n');
+
+
+
+			counter++;
 	}
-	_putchar('\n');
+
 }
