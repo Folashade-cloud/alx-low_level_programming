d79e77e34574fe3e08e54fae20a6faa70d57e104
Folashade-cloud <ftogunajo@gmail.com>
2022-09-14 21:51:26 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in small letters
+ *
+ */
+
+void print_alphabet_x10(void);
+
+{
+
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
