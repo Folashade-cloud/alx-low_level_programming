f26c8ef83b9d7c2041954a9d587d05421a06e12b
Folashade-cloud <ftogunajo@gmail.com>
2022-10-13 08:18:02 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,9 +6,13 @@
 #include <stdarg.h>
 
 int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
 void print_strings(const char *separator, const unsigned int n, ...);
+
 void print_all(const char * const format, ...);
 
 /**
@@ -25,8 +29,11 @@ typedef struct print_formats
 } p_forms;
 
 void print_char(va_list args);
+
 void print_int(va_list args);
+
 void print_float(va_list args);
+
 void print_string(va_list args);
 
 #endif /* VARIADIC_FUNCTIONS_H */
