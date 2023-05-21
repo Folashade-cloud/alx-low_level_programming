96f07c2cb6255ccaf1e2e4d85109586a8000e40f
Folashade-cloud <ftogunajo@gmail.com>
2023-05-21 10:51:41 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print size of computer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+	int i;
+	int li;
+	int ll;
+	int f;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
+	printf("size of an int: %lu byte(s)\n", (unsigned long) sizeof(i));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(li));
+	printf("size of a long long: %lu byte(s)\n", (unsigned long) sizeof(ll);
+	printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
+	return (0);
+}
