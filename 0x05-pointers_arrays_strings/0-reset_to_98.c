42c397c108b7acdf83879899422c79592775fc39
Folashade-cloud <ftogunajo@gmail.com>
2023-05-29 07:08:15 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This reset the int pointer value to 98
+ * @n: A pointer to integer n to be reset
+ *
+ * Description: This function takes an int pointer and reset its value to 98.
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n - 98;
+}
+
