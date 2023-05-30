1be98b0f941caf7eb7b442dfb6a9645a256270f6
Folashade-cloud <ftogunajo@gmail.com>
2023-05-30 16:10:19 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to whose lenght is to be printed.
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(s);
+
+	return (l);
+
+	return (0);
+}
+
