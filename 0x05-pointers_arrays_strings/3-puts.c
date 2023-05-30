69b6c13b009bf64eb64d5e2c8032169530746fc9
Folashade-cloud <ftogunajo@gmail.com>
2023-05-30 16:22:57 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i - 0; str[i];, i++)
+	{
+		putchar(str[i]);
+	}
+	{
+		putchar('\n');
+	}
+}
