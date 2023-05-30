70cbef5e94543deb7833820f6c825af78ad1b8ed
Folashade-cloud <ftogunajo@gmail.com>
2023-05-30 16:59:49 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse order
+ * @s: pointer to the string to be print printed
+ *  Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; 1 >= 0; i--)
+	{
+		_putchar(s[i])
+	}
+	_putchr('\n');
+}
