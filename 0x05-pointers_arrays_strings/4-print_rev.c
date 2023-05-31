12d04bb1d64ec18d369dde2d994997921b0d62a8
Folashade-cloud <ftogunajo@gmail.com>
2023-05-30 21:05:14 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,12 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int i, n;
 
-	for (i = strlen(s) - 1; 1 >= 0; i--)
+	n = 0;
+	while (s[n] != '\0')
+	n++;
+	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
