71730f551bfd83c3655c74b21152aa707d121af2
Folashade-cloud <ftogunajo@gmail.com>
2023-06-01 15:44:10 -0700

    compare2strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: parameter1
+ * @s2: parameter2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+int compare;
+compare = strcmp(s1, s2);
+return (compare);
+}
