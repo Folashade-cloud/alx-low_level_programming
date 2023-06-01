3768b63ef48006a56cac5d817e7cd442f334d06d
Folashade-cloud <ftogunajo@gmail.com>
2023-06-01 15:36:33 -0700

    copy_string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - strncpy copies a string.
+ * @dest: parameter1
+ * @src: parameter2
+ * @n: parameter3
+ * Return: a string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
