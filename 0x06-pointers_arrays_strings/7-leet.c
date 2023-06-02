c760a00f04daf30c3f9094e9f854be8366c4d204
Folashade-cloud <ftogunajo@gmail.com>
2023-06-01 19:57:59 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - a leet conversion function
+ * @x: param x
+ * Return: a string
+ */
+char *leet(char *x)
+{
+int indx1 = 0, indx2;
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+while (x[indx1])
+{
+	for (indx2 = 0; indx2 <= 7; indx2++)
+	{
+		if (x[indx1] == leet[indx2] ||
+		    x[indx1] - 32 == leet[indx2])
+		      x[indx1] = indx2 + '0';
+	}
+	indx1++;
+	}
+	return (x);
+
