58de757b0a99bf769383d1c0188a27e277b583c1
Folashade-cloud <ftogunajo@gmail.com>
2023-06-03 08:03:03 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - Print an integer value
+ * @n: The integer n.
+ */
+void print_number(int n)
+{
+	unsigned int number  = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+
+	if (number > 0)
+		_putchar(number);
+}
