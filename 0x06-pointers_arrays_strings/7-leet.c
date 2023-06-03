99b3447c91b42f8a87945c0dfa9df67a32a0a5b3
Folashade-cloud <ftogunajo@gmail.com>
2023-06-03 06:13:55 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,11 +14,10 @@ while (x[indx1])
 	for (indx2 = 0; indx2 <= 7; indx2++)
 	{
 		if (x[indx1] == leet[indx2] ||
-		    x[indx1] - 32 == leet[indx2])
-		      x[indx1] = indx2 + '0';
+				x[indx1] - 32 == leet[indx2])
+			x[indx1] = indx2 + '0';
 	}
 	indx1++;
-	}
+}
 	return (x);
 }
-
