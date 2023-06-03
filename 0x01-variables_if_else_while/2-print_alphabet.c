6f9cb20b0cf4b415701cbf45465a021517f3e916
Folashade-cloud <ftogunajo@gmail.com>
2023-06-03 09:25:24 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,19 +3,17 @@
 /**
  * main - entry point
  *
- * Description: print letters of alphabet a character per time
- *
  * Return: return 0
  */
 
 int main(void)
-{       
-        char alphabet = 'a';
-        
-        for (alphabet = a; alphabet <= z; alphabet++)
-        {       
-                putchar(alphabet);
-        }               
-        putchar('\n');
-                return 0;
+{
+	char alphabet;
+
+	for (alphabet = a; alphabet <= z; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
 }
