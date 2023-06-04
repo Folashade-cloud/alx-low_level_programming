7d8b2f88f307ac808aea2904fc65b4f128d89fd2
Folashade-cloud <ftogunajo@gmail.com>
2023-06-03 23:09:38 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints number 1 - 9
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+	printf("%d", number);
+	}
+	putchar('\n');
+
+	return (0);
+}
