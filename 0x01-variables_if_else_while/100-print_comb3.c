d3a17c6d71e0dab5d6249c2663e7703b55c93466
Folashade-cloud <ftogunajo@gmail.com>
2023-06-04 01:23:57 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *	(10-99) using putchar
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int tens, unit;
+	int comma = 0, space = 0;
+
+	for (tens = 0; tens < 9; tens++)
+	{
+		for (unit = 1 + tens; unit < 10; unit++)
+		{
+			if (comma != 0 && space != 0)
+			{
+				putchar(comma);
+				putchar(space);
+			}
+			putchar(tens + ('0' - 0));
+			putchar(unit + ('0' - 0));
+			comma = 44, space = 32;	/* ASCII: 44 - ',' | 32 - SPC */
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
