228d9b1be28f61a55a588aebe408c4445cf8d421
Folashade-cloud <ftogunajo@gmail.com>
2023-06-05 15:59:04 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,11 @@
 #define _MAIN_H_
 
 #include <stdio.h>
-int _isupper(int c);
 
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
 
 #endif
