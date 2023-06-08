b9fda46467439ec3f250f6a5144737a0f46c288e
Folashade-cloud <ftogunajo@gmail.com>
2023-06-07 21:13:52 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,24 @@
-Project on recursion, function that itself.
+#include "main.h"
+
+/**
+* _puts_recursion - function that print a string, plus new line
+* @s:string inputed
+* Return:void
+*/
+int _putchar(char c)
+void _puts_recursion(char *s)
+{
+	int counter = 0;
+
+	if (*(s + counter) != '\0')
+{
+	_putchar(*(s + counter));
+	counter++;
+	_puts_recursion(s + counter);
+}
+else
+{
+	_putchar('\n');
+}
+
+}
